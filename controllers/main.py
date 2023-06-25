ea1d25f5229441d16923010f486618daa2b87411
Naznin Akter <naznin.kuet.te@gmail.com>
2023-06-25 06:17:18 +0600

    fix

--- controllers/main.py
+++ controllers/main.py
@@ -19,7 +19,7 @@ class PortalCustomer(http.Controller):
         current_url = request.httprequest.path
         if current_url == '/my/home' or current_url == '/my/orders':
             user = request.env.user
-            sale_orders = request.env['sale.order'].search([('partner_id', '=', request.env.user.partner_id)])
+            sale_orders = request.env['sale.order'].search([('create_uid', '=', request.env.user.id)])
 
             orders = []
             for sale_order in sale_orders:
